61505e4b2f6ddcc955dce5e36d532f731f119373
R1tschY <r1tschy@posteo.de>
2021-01-24 20:51:08 +0100

    Fix discovery

--- src/discover.rs
+++ src/discover.rs
@@ -1,5 +1,8 @@
-use std::path::PathBuf;
+use std::ffi::{OsStr, OsString};
+use std::path::{Path, PathBuf};
 use std::{env, fs};
+
+use nom::Parser;
 
 /// Determine XDG_DATA_DIRS from
 /// https://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
@@ -14,7 +17,10 @@ pub fn get_data_dirs() -> Vec<PathBuf> {
 
     match paths {
         Some(paths) if !paths.is_empty() => paths,
-        _ => vec![PathBuf::from("/usr/local/share"), PathBuf::from("/usr/share")]
+        _ => vec![
+            PathBuf::from("/usr/local/share"),
+            PathBuf::from("/usr/share"),
+        ],
     }
 }
 
@@ -26,39 +32,52 @@ pub fn get_data_home() -> Option<PathBuf> {
     dirs::data_dir()
 }
 
-
-fn collect_files_recursive<T>(path: &PathBuf, filter: T) -> Vec<PathBuf>
-    where T: Fn(&PathBuf) -> bool
-{
+fn collect_files_recursive(path: &Path) -> Vec<PathBuf> {
     let mut result: Vec<PathBuf> = vec![];
-    collect_files_recursive_(path, &mut result, &filter);
+    collect_files_recursive_(path, &mut result);
     result
 }
 
-fn collect_files_recursive_<T>(path: &PathBuf, result: &mut Vec<PathBuf>, filter: &T)
-    where T: Fn(&PathBuf) -> bool
-{
+fn collect_files_recursive_(path: &Path, result: &mut Vec<PathBuf>) {
     if let Ok(dir_entries) = fs::read_dir(path) {
         for dir_entry in dir_entries {
             if let Ok(dir_entry) = dir_entry {
                 let path = dir_entry.path();
-                if path.is_dir() {
-                    collect_files_recursive_(&path, result, filter);
-                } else if filter(&path) {
-                    result.push(path);
+                if let Ok(file_type) = dir_entry.file_type() {
+                    if file_type.is_symlink() {
+                        continue;
+                    } else if file_type.is_dir() {
+                        collect_files_recursive_(&path, result);
+                    } else if path.extension() == Some(OsStr::new("desktop")) {
+                        result.push(path);
+                    }
                 }
             }
         }
     }
 }
 
-pub fn get_desktop_files() -> Vec<PathBuf> {
-    let desktop_ext: PathBuf = ".desktop".into();
-
-    get_data_dirs().iter()
+/// Search for application desktop files in default locations.
+///
+/// Symbolic links are not followed.
+///
+/// Default locations are:
+/// - `applications` subdirectories in `$XDG_DATA_DIRS` or `["/usr/local/share/applications", "/usr/share/applications"]` when `$XDG_DATA_DIRS` is not set
+/// - `$XDG_DATA_HOME/applications` or `~/.local/share/applications` when `$XDG_DATA_HOME` is not set
+pub fn discover_applications() -> Vec<PathBuf> {
+    // TODO: use Desktop File ID to implement precedence order
+    get_data_dirs()
+        .iter()
         .chain(get_data_home().iter())
-        .flat_map(|data_dir| {
-            collect_files_recursive(data_dir, |p| p.ends_with(&desktop_ext))
-        })
+        .flat_map(|data_dir| collect_files_recursive(&data_dir.join("applications")))
+        .collect()
+}
+
+/// Search for desktop files in `dirs`.
+///
+/// Symbolic links are not followed.
+pub fn discover_in_dirs(dirs: &[&Path]) -> Vec<PathBuf> {
+    dirs.iter()
+        .flat_map(|dir| collect_files_recursive(dir))
         .collect()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+//! Library to parse XDG desktop files.
+//!
+//! More information about desktop files and the "Desktop Entry Specification" is available on:
+//! - http://freedesktop.org/wiki/Specifications/desktop-entry-spec
+//! - http://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html
+
+
 #[macro_use(eof, error_position)] extern crate nom;
 #[cfg(test)] #[macro_use] extern crate assert_matches;
 #[cfg(test)] #[macro_use] extern crate indoc;
